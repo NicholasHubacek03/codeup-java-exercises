0477bd70cc54673f904764a4072d92377d2e7989
Nicholas.Hubacek <Nicholas.Hubacek@gmail.com>
2023-04-14 14:38:32 -0500

    first java

---
+++ src/MyFirstClass.java
@@ -0,0 +1,9 @@
+public class MyFirstClass {
+    public static void main(String[] args){
+//This will log stuff in your command line
+//strings in java must be in double quotes(single quotes are reserved for single character
+// you must put a semicolon after every statement or else you will get an error
+        System.out.println("Hello, welcome to java!");
+    }
+
+}

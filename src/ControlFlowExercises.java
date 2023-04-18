6a69d838f6591628724d6d0cbd1714affc76fe47
Nicholas.Hubacek <Nicholas.Hubacek@gmail.com>
2023-04-18 10:47:11 -0500

    Finished the first part of the exercise

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,39 @@
+public class ControlFlowExercises {
+    public static void main (String[] args){
+//        1. Loop Basics
+//        int i = 5;
+//        while (i <= 15) {
+//            System.out.println(i);
+//            i++;
+//        }
+//        1. Do While
+//        int i = 0;
+//        do {
+//            System.out.println(i);
+//            i += 2;
+//        } while (i <= 100);
+//        int i = 100;
+//        do {
+//            System.out.println(i);
+//            i -= 5;
+//        } while (i >= -10);
+//        int i = 2;
+//        do {
+//            System.out.println(i * i);
+//            i++;
+//        } while (i * i < 1000000);
+
+//        1. For
+//        for (int i = 5; i <= 15; i++) {
+//System.out.println(i);
+
+//                for (int i = 100; i >= -10; i -= 5) {
+//System.out.println(i);
+
+//                for (int i = 2; i * i < 1000000; i++) {
+//System.out.println(i * i);
+//        }
+
+    }
+
+}
